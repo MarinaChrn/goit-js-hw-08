f3a81851099737a2dddf2a8d3844199f75d9a853
MarinaChorna <mrnchrn06@gmail.com>
2023-02-25 23:05:56 +0200

    Завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function({seconds}) {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`);}
+, 1000));
+
+const seconds = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(seconds).then(function(seconds) {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
