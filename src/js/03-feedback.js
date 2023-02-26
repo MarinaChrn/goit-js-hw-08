ea0b1482ff4ab1ea76e4f62c3e2ea413f7fa49e8
MarinaChorna <mrnchrn06@gmail.com>
2023-02-26 20:52:26 +0200

    Завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -23,38 +23,3 @@
         
 //     // }
 // });
-
-const LOCAL_KEY = 'feedback-form-state';
-const form = document.querySelector('.feedback-form');
-populateFeedbackForm();
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onInputData, 500));
-
-function onFormSubmit(e) {
-  e.preventDefault();
-  const { email, message } = e.currentTarget.elements;
-  console.log({ email: email.value, message: message.value });
-  if (localStorage.getItem(LOCAL_KEY)) {
-    localStorage.removeItem(LOCAL_KEY);
-  }
-  e.currentTarget.reset();
-}
-
-function onInputData(e) {
-  let data = localStorage.getItem(LOCAL_KEY);
-  data = data ? JSON.parse(data) : {};
-  data[e.target.name] = e.target.value.trim();
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
-}
-
-function populateFeedbackForm() {
-  let data = localStorage.getItem(LOCAL_KEY);
-  if (!data) return;
-  data = JSON.parse(data);
-  // Object.entries(data).forEach(([name, value]) => {
-  //   form.elements[name].value = value || '';
-  // });
-  for (const key in data) {
-    form.elements[key].value = data[key] || '';
-  }
-}
