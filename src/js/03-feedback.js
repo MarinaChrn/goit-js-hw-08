78c5222aed34a2a221d355fe2540cdac70bf2c84
MarinaChorna <mrnchrn06@gmail.com>
2023-02-27 17:37:41 +0200

    Завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,28 @@
-// 1. Отслеживай на форме событие input, и каждый раз записывай в локальное 
-// хранилище объект с полями email и message, в которых 
-// сохраняй текущие значения полей формы. Пусть ключом для хранилища 
-// будет строка "feedback-form-state".
-// 2. При загрузке страницы проверяй состояние хранилища, 
-// и если там есть сохраненные данные, заполняй ими поля формы. 
-// В противном случае поля должны быть пустыми.
-// 3. При сабмите формы очищай хранилище и поля формы, 
-// а также выводи объект с полями email, message и текущими их значениями 
-// в консоль.
-// 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз 
-// в 500 миллисекунд. Для этого добавь в проект и используй библиотеку 
-// lodash.throttle.
+import throttle from 'lodash.throttle';
 
-// const formEl = document.querySelector('.feedback-form');
-// console.log(formEl);
-// const inputEl = formEl.querySelector('input');
-// const textAreaEl = formEl.querySelector('textarea');
-// console.log(formEl);
-// formEl.addEventListener('input', function() {
-//     console.log(inputEl.currentTarget.value);
-//     // const inform = {
-        
-//     // }
-// });
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+let inform = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const { email, message } = formEl.elements;
+reloadPage();
+
+function onInput(e) {
+  inform = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(inform));
+}
+
+function reloadPage() {
+  email.value = inform.email || '';
+  message.value = inform.message || '';
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem('feedback-form-state');
+  e.currentTarget.reset();
+  inform = {};
+}
